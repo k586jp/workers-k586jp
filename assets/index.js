675f056466561418563989c6f6f0b85fac951fd6
k586 <mail@k586.jp>
2025-08-28 00:53:23 +0900

    MD ファイル読込から、REST API を使う方法に変更

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,22 @@
 async function fetchJson() {
 
-    const file = await fetch('main.md');
-    return await file.text();
+    const url = 'https://api.k586.jp/article/id.php';
+
+    const headers = {
+        'Content-Type': 'application/json; charset=UTF-8'
+    };
+    const body = {
+        id: 'profile'
+    };
+    const request = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+
+    const response = await fetch(url, request);
+    const json = await response.json();
+
+    return json.text;
 
 }
