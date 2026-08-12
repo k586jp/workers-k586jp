c9851501d838daafb02463728799dd545abbdc31
k586 <mail@k586.jp>
2026-08-12 09:53:04 +0900

    現地時間変換を試してみる

--- assets/common.js
+++ assets/common.js
@@ -4,11 +4,19 @@
  */
 async function main() {
 
+    const timeFormat = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }
     const timeArray = document.getElementsByTagName('time');
     const length = timeArray.length;
     for (let i = 0; i < length; i++) {
         const utc = new Date(timeArray[i].getAttribute('datetime'));
-        timeArray[i].textContent = utc.toLocaleString(undefined, { dateStyle: "medium", timeStyle: "short" });
+        timeArray[i].textContent = utc.toLocaleString(undefined, timeFormat);
     }
 
     Prism.highlightAll();
